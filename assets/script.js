e94674e85fe17fe109cc68b6637273d8ca4af0a8
Nate Wolfe <wolfe.nate26@gmail.com>
2022-05-21 11:23:12 -0600

    fucntionality and key pulls

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,25 @@ function getCurrent(cityName) {
       console.log(data);
       var lat = data.coord.lat;
       var lon = data.coord.lon;
+      var previous = JSON.parse(localStorage.getItem("weatherUser")) || [];
+      if (previous.indexOf(cityName) === -1) {
+        previous.push(cityName);
+        localStorage.setItem("weatherUser", JSON.stringify(previous));
+      }
+
+      getOneCall(cityName, lat, lon);
+    });
+}
+
+function getOneCall(cityName, lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${Api}&units=imperial`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
     });
 }
 
